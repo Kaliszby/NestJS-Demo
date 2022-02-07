cda36a11f5ea6ee65636311eea4e99813742872f
Kaliszby <sumyung17qwq@gmail.com>
2022-02-07 13:31:59 +0700

    POST With Http Body

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,9 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
 
 @Controller('todo')
 export class TodoController {
   @Get()
   getTodos() {
     return [1, 2, 3, 4];
+  }
+
+  @Post()
+  postTodos(@Body('title') title: string, @Body('subtitle') subtitle: string) {
+    console.log(`title: ${title}, subtitle: ${subtitle}`);
   }
 }
