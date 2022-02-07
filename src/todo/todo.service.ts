f36051074c371e865d3bd170f962d2b1896c833c
Kaliszby <sumyung17qwq@gmail.com>
2022-02-07 14:10:06 +0700

    Entity

--- src/todo/todo.service.ts
+++ src/todo/todo.service.ts
@@ -1,7 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import { Todo } from './todo.entity';
 
 @Injectable()
 export class TodoService {
+  todoArray: Todo[];
+
   addTodo(title: string, subtitle: string) {
     console.log(`Title: ${title}, Subtitle: ${subtitle}`);
   }
